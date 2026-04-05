105bb858009b1dca1b79de38b1224a2bbfd728b1
devdral <84059355+devdral@users.noreply.github.com>
2026-04-05 12:03:58 -0700

    Fix level-end triggers not stopping stopwatch

--- lvl_end_global.gd
+++ lvl_end_global.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@onready var main: CanvasLayer = get_parent().get_node("main")
+@onready var main: CanvasLayer = get_parent().get_node("InGameOverlay")
 
 func _on_body_entered(body: CharacterBody2D) -> void:
 	main.stopwatch_paused = true
