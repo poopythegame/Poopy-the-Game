c33bad2ff33e19d8c018754fc9485a8dd468225e
poopythegame <nindoongi@gmail.com>
2026-06-08 16:41:27 -0700

    ever so slightly revised enemy physics and updated level design

--- scripts/spring.gd
+++ scripts/spring.gd
@@ -5,22 +5,22 @@ extends StaticBody2D
 @onready var area: Area2D = $Area2D
 
 func _ready() -> void:
-    area.body_entered.connect(_contact)
+	area.body_entered.connect(_contact)
 
 func _contact(body: Node2D):
-    print("contact")
-    if body is Player:
-        var player: Player = body
-        var player_pos = player.global_position
-        var delta = player_pos - global_position
-        var dx = abs(delta.x)
-        var dy = abs(delta.y)
-        if dx > dy:
-            delta.y = 0
-        elif dy > dx:
-            delta.x = 0
-        var motion = Vector2.ZERO
-        motion = delta.normalized() * force
-        print(motion)
-        player.motion += motion
-        player.springing = true
+	print("contact")
+	if body is Player:
+		var player: Player = body
+		var player_pos = player.global_position
+		var delta = player_pos - global_position
+		var dx = abs(delta.x)
+		var dy = abs(delta.y)
+		if dx > dy:
+			delta.y = 0
+		elif dy > dx:
+			delta.x = 0
+		var motion = Vector2.ZERO
+		motion = delta.normalized() * force
+		print(motion)
+		player.motion += motion
+		player.springing = true
