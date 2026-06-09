c9a73c9966dcf72d0580033e91bccfc8bc11bdf1
poopythegame <nindoongi@gmail.com>
2026-06-09 09:34:00 -0700

    revised dash panel and enemy logic + level design change

--- scripts/dash_panel.gd
+++ scripts/dash_panel.gd
@@ -6,16 +6,16 @@ extends Node
 @onready var area: Area2D = $Area2D
 
 func _ready() -> void:
-    area.body_entered.connect(_contact)
-    if flip:
-        var sprite: Sprite2D = $Sprite2D
-        sprite.flip_h = true
+	area.body_entered.connect(_contact)
+	if flip:
+		var sprite: Sprite2D = $Sprite2D
+		sprite.flip_h = true
 
 func _contact(body):
-    if body is Player:
-        var player: Player = body
-        var motion: Vector2 = Vector2(1, 0)
-        if flip:
-            motion.x = -1
-        motion *= strength
-        player.motion += motion
+	if body is Player:
+		var player: Player = body
+		var motion: Vector2 = Vector2(1, 0)
+		if flip:
+			motion.x = -1
+		motion *= strength
+		player.motion = motion
