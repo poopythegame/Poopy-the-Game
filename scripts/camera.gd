1fa94030e37a43fc992f8f5313dc304299170431
devdral <84059355+devdral@users.noreply.github.com>
2026-06-22 12:09:58 -0700

    Camera lookahead

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,41 +1,76 @@
 extends Camera2D
 
+## How far the horizontal (x) offset shifts the camera to show what's in front of the player.
+@export var x_offset_amount: float = 75
+## How long it takes for the horizontal offset to shift the camera.
+@export_custom(PROPERTY_HINT_NONE, "suffix:s") var x_offset_time: float = .5
+## The easing curve for horizontal offset changes.
+@export_exp_easing var x_offset_ease: float = -2
+
 var player: Player
 var left_boundary: Node2D
 var right_boundary: Node2D
+var x_offset: float = 0
+var next_x_offset: float = 0
+var x_offset_t: float = 0
+var curr_x_offset: float = 0
 
 func _ready() -> void:
-    var candidates := get_tree().get_nodes_in_group("cam_boundary")
-    print(len(candidates))
-    for node in candidates:
-        if node.flip:
-            if right_boundary:
-                push_error("Cannot have more than one camera left-boundary.")
-            right_boundary = node
-        else:
-            print("madeit")
-            if left_boundary:
-                push_error("Cannot have more than one camera left-boundary.")
-            left_boundary = node
-    player = get_tree().get_first_node_in_group("Player")
+	var candidates := get_tree().get_nodes_in_group("cam_boundary")
+	print(len(candidates))
+	for node in candidates:
+		if node.flip:
+			if right_boundary:
+				push_error("Cannot have more than one camera left-boundary.")
+			right_boundary = node
+		else:
+			if left_boundary:
+				push_error("Cannot have more than one camera left-boundary.")
+			left_boundary = node
+	player = get_tree().get_first_node_in_group("Player")
 
 func get_rect() -> Rect2:
-    var pos := global_position
-    var viewport_size := get_viewport_rect().size
-    var world_size := viewport_size / zoom
-    var top_left := pos - (world_size / 2.)
-    return Rect2(top_left, world_size)
+	var pos := global_position
+	var viewport_size := get_viewport_rect().size
+	var world_size := viewport_size / zoom
+	var top_left := pos - (world_size / 2.)
+	return Rect2(top_left, world_size)
 
-func _process(_delta: float) -> void:
-    global_position = player.global_position
-    var rect := get_rect()
-    var left_x := rect.position.x
-    var right_x := left_x + rect.size.x
-    var shift_amount := 0.
-    if left_boundary:
-        if left_boundary.global_position.x > left_x:
-            shift_amount = left_boundary.global_position.x - left_x
-    if right_boundary:
-        if right_boundary.global_position.x < right_x:
-            shift_amount = right_boundary.global_position.x - right_x
-    global_position.x += shift_amount
+func _process(delta: float) -> void:
+	var prev_next_offset = next_x_offset
+	if should_disable():
+		next_x_offset = 0
+	elif player.velocity.x > Player.topspeed:
+		next_x_offset = x_offset_amount
+	elif player.velocity.x < -Player.topspeed:
+		next_x_offset = -x_offset_amount
+	else:
+		next_x_offset = 0
+	if prev_next_offset != next_x_offset and x_offset_t > 0 and x_offset_t < x_offset_time:
+		x_offset = curr_x_offset
+		x_offset_t = 0
+	curr_x_offset = x_offset
+	if next_x_offset != x_offset:
+		var t = x_offset_t / x_offset_time
+		curr_x_offset = lerp(x_offset, next_x_offset, ease(t, x_offset_ease))
+		x_offset_t += delta
+		if x_offset_t > x_offset_time:
+			x_offset_t = 0
+			x_offset = next_x_offset
+	global_position = Vector2(player.global_position.x + curr_x_offset, player.global_position.y)
+	var rect := get_rect()
+	var left_x := rect.position.x
+	var right_x := left_x + rect.size.x
+	var shift_amount := 0.
+	if left_boundary:
+		if left_boundary.global_position.x > left_x:
+			shift_amount = left_boundary.global_position.x - left_x
+	if right_boundary:
+		if right_boundary.global_position.x < right_x:
+			shift_amount = right_boundary.global_position.x - right_x
+	global_position.x += shift_amount
+
+func should_disable() -> bool:
+	if player.is_grappling:
+		return true
+	return false
