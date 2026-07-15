f4349dd8f789fd525b0b3833c21c3b9985845c24
devdral <84059355+devdral@users.noreply.github.com>
2026-07-14 17:47:39 -0700

    New HUD (pt. 1)

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -8,49 +8,39 @@ class_name InGameOverlay
 @onready var rankings_scene: PackedScene = load("uid://cpvt7jfaq7yjl")
 
 @onready var player: CharacterBody2D = get_parent().get_node("Player")
-@onready var horizontal_speed: Label = $left/HorizontalSpeed
-@onready var vertical_speed: Label = $right/VerticalSpeed
-@onready var p_rank: Label = $"right/p-rank"
-@onready var healthbar: ProgressBar = $left/HealthBar
-@onready var health_label: Label = $left/HealthBar/HealthNumber
-@onready var minutes_label: Label = $left/HBoxContainer/minutes
-@onready var seconds_label: Label = $left/HBoxContainer/seconds
-@onready var msec_label: Label = $left/HBoxContainer/msec
+@onready var horizontal_speed: Label = $Left/HorizontalSpeed/Readout
+@onready var vertical_speed: Label = $Right/VerticalSpeed/Readout
+@onready var healthbar: ProgressBar = $Left/HealthBar
+@onready var health_label: Label = $Left/HealthBar/HealthNumber
+@onready var time_label: Label = $Left/Time/Readout
 @onready var whiteout: ColorRect = $Whiteout
 
 var stopwatch_paused := true
-@onready var coins_label: Label = $left/coins
+@onready var coins_label: Label = $Left/Coins/Readout
 var time: float = 0.0
 var minutes: int = 0
 var seconds: int = 0
-var mseconds: int = 0 
+var millis: int = 0 
 
 func _process(delta): 
 	var level_index = Global.current_level
 	var coins = Global.get_coins()
-	coins_label.text = "coins: %d" % coins
+	coins_label.text = "%d" % coins
 	if not stopwatch_paused:
 		time += delta
-		mseconds = fmod(time, 1) * 1000
+		millis = fmod(time, 1) * 1000
 		seconds = fmod(time, 60)
-		minutes = fmod(time, 3600) / 60 
-		minutes_label.text = "%02d:" % minutes
-		seconds_label.text = "%02d." % seconds
-		msec_label.text = "%03d" % mseconds
-	var rank = Global.get_rank()
-	if rank:
-		p_rank.show()
-		p_rank.text = "p-rank: %s" % Global.levels.levels[Global.current_level].rankings[rank].letter
-	else:
-		p_rank.hide()
+		minutes = fmod(time, 3600) / 60
+		var time_readout = "%02d:%02d.%03d" % [minutes, seconds, millis]
+		time_label.text = time_readout
 
 func display_speed(x, y):
 	var raw_vspeed = abs(y) - 50
 	var raw_hspeed = abs(x)
 	var vspeed = raw_vspeed / 30.0 # 30 px = 1 m
 	var hspeed = raw_hspeed / 30.0
-	horizontal_speed.text = "%0.1f m/s" % hspeed
-	vertical_speed.text = "%0.1f m/s" % vspeed
+	horizontal_speed.text = "%0.1f" % hspeed
+	vertical_speed.text = "%0.1f" % vspeed
 
 func log_health(value: float, max: float) -> void:
 	health_label.text = str(int(value))
