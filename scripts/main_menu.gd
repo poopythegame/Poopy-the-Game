41d32a3489b3cdc30c1aff8c18d877aaf9d72d56
devdral <84059355+devdral@users.noreply.github.com>
2026-07-18 16:53:01 -0700

    TItle animation pt. 5

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -98,8 +98,9 @@ func title_poopy_jump(t: float) -> void:
 	if t <= 0.03:
 		title_poopy_jump_vel -= 10
 	title_poopy.position.y += title_poopy_jump_vel
-	if title_poopy.position.y > 310:
-		title_poopy.position.y = 310
+	if title_poopy.position.y > 330:
+		title_poopy.position.y = 330
+	print(title_poopy.position.y)
 	title_poopy_jump_vel += 50 * (t - title_poopy_last_frametime)
 	title_poopy_last_frametime = t
 
@@ -115,19 +116,22 @@ func title_poopy_run(duration: float) -> Tween:
 		tween.tween_callback(func(): title_poopy.rotation_degrees -= 90)
 		tween.tween_property(title_poopy, "global_position:x", 90, segment_duration)
 		tween.tween_callback(func(): title_poopy.rotation_degrees -= 90)
-		tween.tween_property(title_poopy, "global_position:y", screen_rect.size.y - 80, segment_duration)
+		tween.tween_property(title_poopy, "global_position:y", screen_rect.size.y - 60, segment_duration)
 		tween.tween_callback(func(): title_poopy.rotation_degrees -= 90)
 		tween.tween_property(title_poopy, "global_position:x", screen_rect.size.x / 2, segment_duration / 2)
-		tween.tween_callback(func(): title_poopy.play("armflap"))
+	tween.tween_callback(func(): title_poopy.play("armflap"))
 	return tween
 
 func title_begin_title_reveal():
 	var tween := create_tween()
 	title_title_reveal_tween = tween
-	tween.tween_interval(2)
+	tween.tween_interval(4)
 	tween.tween_callback(func(): title_logo.modulate.a = 1)
 	tween.parallel().tween_property(title_logo, "offset_transform_scale", Vector2(1, 1), 0.5)
 	tween.parallel().tween_method(screen_shake, 10, 5, 0.35).set_delay(0.4)
+	tween.tween_callback(func(): title_logo.modulate.a = 0)
+	tween.tween_callback(func(): title_logo.modulate.a = 1).set_delay(.05)
+	tween.tween_interval(.05)
 	tween.tween_callback(whiteout.show)
 	tween.parallel().tween_property(whiteout, "modulate:a", 1, 0.2)
 	tween.tween_callback(func():
@@ -140,34 +144,34 @@ func title_begin_title_reveal():
 		title_poopy.play("jump")
 		var tween_drop_infobox := create_tween()
 		tween_drop_infobox.tween_callback(title_info_box.show).set_delay(19)
-		tween_drop_infobox.tween_property(title_info_box, "offset_transform_position:y", 0, 1)
+		tween_drop_infobox.tween_property(title_info_box, "offset_transform_position:y", -10, 1)
 		tween_drop_infobox.tween_property(title_info_box, "offset_transform_position:y", -50, .1)
 		tween_drop_infobox.tween_interval(.05)
-		tween_drop_infobox.tween_property(title_info_box, "offset_transform_position:y", 0, .1)
+		tween_drop_infobox.tween_property(title_info_box, "offset_transform_position:y", -10, .1)
 		tween_drop_infobox.set_trans(Tween.TRANS_CUBIC)
 		)
 	tween.tween_property(whiteout, "modulate:a", 0, 0.2)
 	tween.tween_property(title_poopy, "scale", Vector2(0.3, 0.3), 0.5)
 	tween.parallel().tween_method(title_poopy_jump, 0., 1., .5)
 	tween.tween_callback(func(): title_poopy.play("idle"))
-	tween.tween_interval(5.15 - 1.74)
+	tween.tween_interval(3.50 - 1.20)
+	tween.tween_callback(func(): title_poopy.play("turn_around"))
+	tween.tween_interval(2./6)
 	tween.tween_property(whiteout, "modulate:a", 1, 0.2)
 	tween.tween_callback(func():
 		title_portraits_background.show()
 		title_portraits_background.process_mode = Node.PROCESS_MODE_INHERIT)
 	tween.tween_property(whiteout, "modulate:a", 0, 0.2)
-	tween.tween_callback(func(): title_poopy.play("turn_around"))
-	tween.tween_interval(2./6)
 	tween.tween_callback(func(): title_poopy.play("armflap"))
-	tween.tween_interval(8.70 - 5.15)
+	tween.tween_interval(8.70 - 3.50)
 	tween.tween_callback(title_poopy.play.bind("dance1"))
-	tween.tween_interval(10.15 - 8.70)
+	tween.tween_interval(9.85 - 8.70)
 	tween.tween_callback(title_poopy.play.bind("armflap"))
-	tween.tween_interval(11.30 - 10.15)
-	tween.tween_subtween(title_poopy_run(1.20))
-	# tween.tween_interval(12.50 - 10.85)
+	tween.tween_interval(11.30 - 9.85)
+	tween.tween_subtween(title_poopy_run(1.10))
+	# tween.tween_interval(12.40 - 10.85)
 	tween.tween_callback(title_poopy.play.bind("armflap"))
-	tween.tween_interval(13.90 - 12.50)
+	tween.tween_interval(13.90 - 12.40)
 	tween.tween_callback(title_poopy.play.bind("dance3"))
 	tween.tween_interval(15.05 - 13.90)
 	tween.tween_callback(title_poopy.play.bind("armflap"))
