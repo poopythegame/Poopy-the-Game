860e53f75fdc13828eab6dc6a335cce413f218e8
devdral <84059355+devdral@users.noreply.github.com>
2026-07-28 17:31:52 -0700

    Level jingles (missing title card)

--- scripts/level_desc.gd
+++ scripts/level_desc.gd
@@ -3,6 +3,7 @@ class_name LevelDesc
 
 @export var title: String
 @export var thumbnail: Texture2D
+@export var jingle: AudioStream
 @export var music: AudioStream
 @export var scene: PackedScene
 @export var rankings: Array[RankDef]
