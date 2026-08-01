89767aa410c8d6a220719cdff8be57ed5aa7fd96
Devon Albert <devon.drake.albert@outlook.com>
2026-07-31 22:43:49 -0700

    Fix title Poopy jump not always landing in the right spot

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -159,8 +159,6 @@ func _input(event: InputEvent) -> void:
 			change_screen(Screen.MENU, false, Transition.WIPE)
 
 func title_poopy_jump(t: float) -> void:
-	if t <= 0.05:
-		title_poopy_jump_vel -= 10
 	title_poopy.position.y += title_poopy_jump_vel
 	if title_poopy.position.y > 330:
 		title_poopy.position.y = 330
@@ -187,6 +185,7 @@ func title_poopy_run(duration: float) -> Tween:
 
 func title_begin_title_reveal():
 	var tween := create_tween()
+	title_poopy_jump_vel = -10
 	title_title_reveal_tween = tween
 	tween.tween_callback(play_audio.bind(trumpet_sfx))
 	tween.tween_interval(4 - trumpet_sfx[0].get_length())
