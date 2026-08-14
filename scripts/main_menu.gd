451122567b6ac2551330d0fc7ee75df538a2c1d7
devdral <84059355+devdral@users.noreply.github.com>
2026-08-14 08:53:03 -0700

    Add "Join the Offical Discord" menu option

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,8 @@
 extends Control
 class_name MainMenu
 
+const DISCORD_LINK: String = "https://discord.gg/AAUyDqRbS"
+
 @export var labels_slide_time := 2.
 @export var start_screen := Screen.TITLE
 @export_group("Sounds")
@@ -146,6 +148,8 @@ func _on_menu_option_selected(index: int):
 		change_screen(Screen.OPTIONS, true, Transition.CROSSFADE)
 	elif index == 3:
 		change_screen(Screen.CHARACTERS, true, Transition.CROSSFADE)
+	elif index == 4:
+		OS.shell_open(DISCORD_LINK)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("back"):
