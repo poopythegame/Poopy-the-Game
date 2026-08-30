ee42a957790a334b52056a4ff413a69735a0c661
devdral <84059355+devdral@users.noreply.github.com>
2026-08-29 20:53:31 -0700

    Fix being able to play video from anywhere

--- scripts/tutorial_billboard.gd
+++ scripts/tutorial_billboard.gd
@@ -4,11 +4,13 @@ extends Sprite2D
 
 @onready var video_stream_player: VideoStreamPlayer = $VideoStreamPlayer
 @onready var area_2d: Area2D = $Area2D
+var player: Player
 
 func _ready() -> void:
 	video_stream_player.stream = video
+	player = get_tree().get_first_node_in_group("Player")
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("up"):
+	if event.is_action_pressed("up") and player in area_2d.get_overlapping_bodies():
 		video_stream_player.show()
 		video_stream_player.play()
